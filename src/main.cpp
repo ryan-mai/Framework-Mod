e38099cbdf0c12aaa1dbe8900763e88591e27932
Ryan Mai <ryanmai757@gmail.com>
2025-11-21 20:37:50 -0500

    I began integrating the LCD library with the parse data i got from the weather api

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,69 @@
 #include <Arduino.h>
 
 #include <ArduinoJson.h>
-#include <HttpClient.h>
+#include <HTTPClient.h>
 #include <WiFi.h>
 
-#include <TimeLib.h>
+// #include <TimeLib.h>
 
 #include <Wire.h>
-#include <LiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
 
 #include "secrets.h"
+const char* city = "Tokyo";
 
-const char* apiKey = "if i commit my api key ill rotate it hopefully";
-const char* city = "Tokyo,jp";
-
-const char* server = "https://api.weatherapi.com/v1/current.json";
 // Setup
-// LiquidCrystal_I2C lcd(0x3F, 16, 2);
+LiquidCrystal_I2C LCD(0x3F, 16, 2);
 
 unsigned long lastUpdated = 0;
 const long updateInterval = 600000; // 10 mins;
 const long connectionDelay = 500;
 
 void getWeather() {
+  Serial.println("Accessing the weather api");;
   if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
+    String url = "https://api.weatherapi.com/v1/current.json?key=" + String(apiKey) + "&q=" + city + "&aqi=no";;
+
     WiFiClientSecure client;
     client.setInsecure();
 
-    String url = String(server) + "?key=" + apiKey + "&q=" + city + "&aqi=no";
-    
+    HTTPClient http;
+
+    Serial.println("Using the weather url: ");
+    Serial.println(url);
     if (http.begin(client, url)) {
       int httpCode = http.GET();
+
       if (httpCode > 0) {
-        String data = http.getString();
-        Serial.println(data);
+        String payload = http.getString();
+        Serial.println(payload);
+
+        JsonDocument doc;
+
+        DeserializationError error = deserializeJson(doc, client);
+        if (error) {
+          Serial.print(F("deseriializeJson() failed"));
+          Serial.println(error.c_str());
+          return;
+        }
+        const char* region = doc["location"]["region"];
+        float temp_c = doc["current"]["temp_c"];
+        float feelslike_c = doc["current"]["feelslike_c"];
+        const char* condition = doc["current"]["condition"]["text"];
+        float wind_kph = doc["current"]["wind_kph"];
+
+        LCD.setCursor(0, 0);
+        LCD.printf("%.13s", region);
+
+        LCD.setCursor(14, 1);
+        LCD.printf("%.0fC", temp_c);
+
+        LCD.setCursor(0, 1);
+        LCD.printf("%.14s", condition);
+
+        Serial.printf("%s, | %.1f°C (feels %.1f°C) | %s | Wind %.1f\n",
+                      region, temp_c, feelslike_c, condition, wind_kph
+        );
       } else {
         Serial.printf("http.GET() failed, error %d\n", httpCode);
       }
@@ -52,8 +81,18 @@ String nf(int num) {
   return String(num);
 }
 
-String formateDate() {
-  return String(year()) + "-" + nf(month()) + "-" + nf(day()) + " " + nf(hour()) + ":" + nf(minute()) + ":" + nf(second());
+// String formateDate() {
+//   return String(year()) + "-" + nf(month()) + "-" + nf(day()) + " " + nf(hour()) + ":" + nf(minute()) + ":" + nf(second());
+// }
+
+void spinner() {
+  static int8_t counter = 0;
+  const char* c = "\xa1\xa5\xdb";
+  LCD.setCursor(15, 1);
+  LCD.print(c[counter + 1]);
+  if (counter == strlen(c)) {
+    counter = 0;
+  }
 }
 
 void setup() {
@@ -63,29 +102,38 @@ void setup() {
 
   WiFi.begin(ssid, password);
 
+  LCD.init();
+  LCD.backlight();
+  LCD.setCursor(0, 0);
+  
   while (WiFi.status() != WL_CONNECTED) {
     delay(connectionDelay);
     Serial.println("Connecting to WiFi...");
+    spinner();
   }
 
-  Serial.println("Connected to the WiFi!");
   Serial.println("");
-  Serial.println("IP address: ");
+  Serial.println("Connected to the WiFi!");
+  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-  // lcd.init();
-  // lcd.backlight();
-  // lcd.clear();
-  // lcd.setCursor(0, 0);
+
+  LCD.clear();
+  LCD.setCursor(0, 0);
+  LCD.println("Online");
+  LCD.setCursor(0, 1);
+
+  getWeather();
+  lastUpdated = millis();
 }
 
 void loop() {
-  if (Serial.available()) {
-    char t = Serial.read();
-    if (t == 'T') {
-      unsigned long timestamp = Serial.parseInt();
-      setTime(timestamp);
-    }
-  }
+  // if (Serial.available()) {
+  //   char t = Serial.read();
+  //   if (t == 'T') {
+  //     unsigned long timestamp = Serial.parseInt();
+  //     setTime(timestamp);
+  //   }
+  // }
 
   if (millis() - lastUpdated > updateInterval) {
     getWeather();
